c511b2af75bebbc157a9ce806ca6ad91eb81b8e9
李彦 <844698022@qq.com>
2023-04-28 19:02:17 +0800

    ✨feat: add update functional

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { cac } from "cac";
 import chalk from "chalk";
 import pkg from "../package.json";
-import { detProject, listProjects, openProject, setClinetName, setProject } from "./utils";
+import { detProject, listProjects, openProject, setClinetName, setProject, updateConfig } from "./utils";
 
 const cli = cac();
 
 cli
-  .command("add <name> [path]", "add a link")
+  .command("add <name> [path] [desc]", "add a link")
   .example("lk add uc /Users/xxx/uc")
-  .action((name, path) => {
-    setProject(name, path);
+  .action((name, path, desc) => {
+    setProject(name, path, desc);
   });
 
 cli.command("list", "list all links").action(() => {
@@ -20,14 +20,16 @@ cli.command("remove <name>", "remove link").action((name) => {
   detProject(name);
 });
 
-cli
-  .command("<name>", "open link")
-  .action((name) => {
-    openProject(name);
-  });
+cli.command("<name>", "open link").action((name) => {
+  openProject(name);
+});
 
 cli.command("set-client <clientName>", "set client name").action((name) => {
   setClinetName(name);
+});
+
+cli.command("update-config").action(() => {
+  updateConfig();
 });
 
 // Listen to unknown commands
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,11 @@ import { existsSync, readFileSync, writeFileSync } from "fs";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
+interface Project {
+  path: string;
+  desc?: string;
+}
+
 const projectsFile = "projects.json";
 const initFile = "config.json";
 
@@ -19,7 +24,7 @@ function getConfigFile(file: string): string {
 }
 
 //读取配置文件
-function readConfigFile(file: string): Record<string, string> {
+function readConfigFile(file: string): Record<string, Project> {
   const configFile = getConfigFile(file);
   if (existsSync(configFile)) {
     const content = readFileSync(configFile, "utf-8");
@@ -29,8 +34,8 @@ function readConfigFile(file: string): Record<string, string> {
 }
 
 // 设置项目
-export function setProject(name: string, path?: string): void {
-  if (!path) {
+export function setProject(name: string, path?: string, desc?: string): void {
+  if (!path || path === ".") {
     path = process.cwd();
   }
   if (!existsSync(path)) {
@@ -39,7 +44,12 @@ export function setProject(name: string, path?: string): void {
   }
   const configFile = getConfigFile(projectsFile);
   const content = readConfigFile(projectsFile);
-  const json = Object.assign(content, { [name]: path });
+  const json = Object.assign(content, {
+    [name]: {
+      path,
+      desc,
+    },
+  });
   writeFileSync(configFile, JSON.stringify(json));
   console.log(chalk.greenBright(`${name}已设置成功`));
 }
@@ -50,11 +60,11 @@ export function listProjects(): void {
   const keys = Object.keys(content);
   if (keys.length === 0) {
     console.log(chalk.yellowBright("您还没有设置此link"));
-    console.log(chalk.yellowBright("设置项目: lk add <alias> <path>"));
+    console.log(chalk.yellowBright("设置项目: lk add <alias> [path] [desc]"));
     return;
   }
   keys.forEach((key) => {
-    console.log(chalk.greenBright(`${key}: ${content[key]}`));
+    console.log(chalk.greenBright(`${key}: ${[content[key].desc, content[key].path].join("，")}`));
   });
 }
 
@@ -70,18 +80,18 @@ export function detProject(alias: string): void {
 // 打开项目
 export function openProject(alias: string) {
   const content = readConfigFile(projectsFile);
-  const path = content[alias];
-  if (!path) {
+  const project = content[alias];
+  if (!project) {
     console.log(`请先设置${alias}的路径: ${chalk.greenBright(`lk add ${alias} <path>`)}`);
     return;
   }
-  if (!existsSync(path)) {
-    console.log(chalk.red(`文件或文件夹不存在: ${path}`));
+  if (!existsSync(project.path)) {
+    console.log(chalk.red(`文件或文件夹不存在: ${project}`));
     return;
   }
   const config = readConfigFile(initFile);
   const clientName = config.clientName || "code";
-  const { stderr } = execaSync(`${clientName} ${path}`);
+  const { stderr } = execaSync(`${clientName} ${project.path}`);
   if (stderr) {
     console.log(chalk.red(stderr));
   } else {
@@ -96,4 +106,23 @@ export function setClinetName(name: string) {
   const json = Object.assign(content, { clientName: name });
   writeFileSync(configFile, JSON.stringify(json));
   console.log(chalk.greenBright(`客户端已设置为: ${name}`));
+}
+
+export function updateConfig() {
+  const content = readConfigFile(projectsFile);
+  const keys = Object.keys(content);
+  if (keys.length === 0) {
+    return;
+  }
+  keys.forEach((key) => {
+    if (typeof content[key] === "string") {
+      content[key] = {
+        path: content[key] as unknown as string,
+      };
+    }
+  });
+
+  const configFile = getConfigFile(projectsFile);
+  writeFileSync(configFile, JSON.stringify(content));
+  console.log(chalk.greenBright("更新成功"));
 }
